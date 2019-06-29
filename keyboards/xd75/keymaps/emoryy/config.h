d7e952855455a439b87e0a2b70631b510b5fb514
Barta Gábor <emoryy@gmail.com>
2019-06-29 16:12:55 +0200

    XD75 emoryy keymap

---
+++ keyboards/xd75/keymaps/emoryy/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// place overrides here
+
+#undef RGBLIGHT_HUE_STEP
+#define RGBLIGHT_HUE_STEP 1
+#undef RGBLIGHT_SAT_STEP
+#define RGBLIGHT_SAT_STEP 5
+#undef RGBLIGHT_VAL_STEP
+#define RGBLIGHT_VAL_STEP 5
+#define RGBLIGHT_SLEEP
+
+#define TAPPING_TERM 200
